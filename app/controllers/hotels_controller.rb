ca366efb2aca8641d8f3a803d42434c91483f0c5
Nikorexio <lionrootsrussia@gmail.com>
2025-05-15 23:09:09 +0300

    Исправлен поиск, теперь в результатах отсутствуют дубликаты

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -6,52 +6,19 @@ class HotelsController < ApplicationController
     @q = Hotel.ransack(params[:q])
     @q.sorts = 'created_at desc' if @q.sorts.empty?
 
+    search_relation = @q.result(distinct: true)
+                         .includes(:city, :rooms, :reviews)
+                         .with_attached_images
+    
     @pagy, @hotels = pagy(
-      @q.result(distinct: true)
-        .includes(:city, :rooms)
-        .with_attached_images,
+      search_relation,
       items: 12
     )
-
-    if params[:q]&.dig(:rooms_room_type_eq).present?
-      room_type = params[:q][:rooms_room_type_eq]
-      @hotels = @hotels.joins(:rooms)
-                       .where(rooms: { room_type: room_type })
-                       .distinct
-    end
-
-    if params[:q]&.dig(:rooms_room_price_gteq).present? || 
-      params[:q]&.dig(:rooms_room_price_lteq).present? ||
-      params[:q]&.dig(:rooms_room_capacity_gteq).present?
- 
-      @hotels = @hotels.joins(:rooms)
-      
-      if params[:q][:rooms_room_price_gteq].present?
-        @hotels = @hotels.where("rooms.price >= ?", params[:q][:rooms_price_gteq])
-      end
-      
-      if params[:q][:rooms_room_price_lteq].present?
-        @hotels = @hotels.where("rooms.price <= ?", params[:q][:rooms_price_lteq])
-      end
-  
-      if params[:q][:rooms_room_capacity_gteq].present?
-        @hotels = @hotels.where("rooms.capacity >= ?", params[:q][:rooms_capacity_gteq])
-      end
-  
-      @hotels = @hotels.distinct
-    end
   end
 
   def log_action
     Rails.logger.info("Action logged: #{params[:action_name]}")
     head :ok
-  end
-
-  def search
-    @q = Hotel.ransack(params[:q])
-    @pagy, @hotels = pagy(@q.result.includes(:city), items: 12)
-    
-    render :index
   end
 
   # GET /hotels/1
